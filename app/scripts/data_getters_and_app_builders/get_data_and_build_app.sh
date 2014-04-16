1734c404884788a6770c47fc0b3d7fd7cb2f4ad8
vssystemluba <mixter.c@gmail.com>
2014-04-15 21:12:22 -0700

    changes to build script on server

--- app/scripts/data_getters_and_app_builders/get_data_and_build_app.sh
+++ app/scripts/data_getters_and_app_builders/get_data_and_build_app.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
+cd /home/mico2178/git_projects/PlatTheLeague/app/scripts/data_getters_and_app_builders
 python get_all_champs.py
 grunt build
-rm -rf /var/www/public_html/app
-cp -r ../../dist /var/www/public_html/app
+rm -rf /home/mico2178/public_html/app/*
+cp -r ../../../dist/* /home/mico2178/public_html/app
